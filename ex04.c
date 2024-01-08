28abc3cf0f4d14783f50ef9c1b34609038aefbfa
Anndrea31 <155971235+Anndrea31@users.noreply.github.com>
2024-01-08 16:22:59 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+
+ int main ()
+ {
+int somma;
+int num1;
+int num2;
+printf("inserisci il primo valore");
+scanf("%d", &num1);
+printf("inserisci il secondo valore");
+ scanf ("%d", &num2);
+ somma=num1 + num2;
+ printf("la somma dei due numeri è:%d +%d = %d  ",num1,num2,somma);
+return 0;
+ }
