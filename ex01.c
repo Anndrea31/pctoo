f516470f8adb6cb9dfe4c35016598e5e2d772459
Anndrea31 <155971235+Anndrea31@users.noreply.github.com>
2024-01-08 15:13:31 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+int intero = 1;
+int b = 8943;
+int c = 143;
+printf("intero:%d\n",intero);
+printf("b:%d\n",b);
+printf("c:%d\n",c);
+
+return 0;
+}
---
+++ ex02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+float o = 28; //flag ; %f
+char e = 'E'; //flag ; %c
+char l = 'L'; //flag ; %c
+int q = 11; //flag ; %c
+printf("float:%f o",o);
+printf("char:%c e",e);
+printf("char:%c l",l);
+printf("int:%c q",q);
+return 0;
+}
